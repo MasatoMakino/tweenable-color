3eb77b3f56cf1b8fceae797b1c881289e1f12dbc
MasatoMakino <makino.masato.g@gmail.com>
2023-01-25 15:20:09 +0900

    add : TweenableHSL.spec.ts

--- __test__/TweenableColor.spec.ts
+++ __test__/TweenableColor.spec.ts
@@ -1,10 +1,7 @@
 import TWEEN from "@tweenjs/tween.js";
 import { TweenableColor } from "../src";
-import * as FakeTimers from "@sinonjs/fake-timers";
 
 describe("TweenableColor", () => {
-  const clock = FakeTimers.install();
-
   beforeEach(() => {
     TWEEN.removeAll();
     TWEEN.update(0);
@@ -13,6 +10,15 @@ describe("TweenableColor", () => {
   test("new TweenableColor", () => {
     const color = new TweenableColor();
     expect(color).toBeTruthy();
+  });
+
+  test("tween", () => {
+    const color = new TweenableColor();
+    expect(color.getAttribute()).toEqual([0, 0, 0, 1]);
+
+    color.change(255, 255, 255, 1.0, 1, { startTime: 0 });
+    TWEEN.update(1);
+    expect(color.getAttribute()).toEqual([1, 1, 1, 1]);
   });
 
   test("clone", () => {
@@ -24,8 +30,40 @@ describe("TweenableColor", () => {
 
     color.change(255, 255, 255, 1.0, 1, { startTime: 0 });
     TWEEN.update(1);
-
-    expect(color.getAttribute()).toEqual([1, 1, 1, 1]);
     expect(clone.getAttribute()).toEqual([0, 0, 0, 1]);
+  });
+
+  test("getAlpha", () => {
+    const color = new TweenableColor();
+    expect(color.getAlpha()).toEqual("1");
+
+    color.change(255, 255, 255, 0.0, 1, { startTime: 0 });
+    TWEEN.update(0.5);
+    expect(color.getAlpha()).toEqual("0.5");
+
+    TWEEN.update(1.0);
+    expect(color.getAlpha()).toEqual("0");
+  });
+
+  test("getCSSStyle", () => {
+    const color = new TweenableColor();
+    expect(color.getCSSStyle()).toEqual("rgba(0,0,0,1)");
+
+    const white = new TweenableColor(255, 255, 255);
+    expect(white.getCSSStyle()).toEqual("rgba(255,255,255,1)");
+
+    const half = new TweenableColor(200.4, 200.5, 200.6);
+    expect(half.getCSSStyle()).toEqual("rgba(200,201,201,1)");
+  });
+
+  test("getCSSColor", () => {
+    const color = new TweenableColor();
+    expect(color.getCSSColor()).toEqual("rgb(0,0,0)");
+
+    const white = new TweenableColor(255, 255, 255);
+    expect(white.getCSSColor()).toEqual("rgb(255,255,255)");
+
+    const half = new TweenableColor(200.4, 200.5, 200.6);
+    expect(half.getCSSColor()).toEqual("rgb(200,201,201)");
   });
 });
---
+++ __test__/TweenableHSL.spec.ts
@@ -0,0 +1,36 @@
+import TWEEN from "@tweenjs/tween.js";
+import { TweenableHSL } from "../src";
+
+describe("TweenableHSL", () => {
+  beforeEach(() => {
+    TWEEN.removeAll();
+    TWEEN.update(0);
+  });
+
+  test("change black to white", () => {
+    const color = new TweenableHSL();
+    color.change(255, 255, 255, 0.0, 1, { startTime: 0 });
+
+    TWEEN.update(0.5);
+    expect(color.getAttribute()).toEqual([0.5, 0.5, 0.5, 0.5]);
+
+    TWEEN.update(1);
+    expect(color.getAttribute()).toEqual([1, 1, 1, 0.0]);
+  });
+
+  test("change h:0 to h:180", () => {
+    const color = new TweenableHSL(255, 0, 0);
+    color.change(0, 255, 255, 1, 1, { startTime: 0 });
+
+    TWEEN.update(0.5);
+    expect(color.getAttribute()[0]).toBeCloseTo(0.5);
+    expect(color.getAttribute()[1]).toBeCloseTo(1.0);
+    expect(color.getAttribute()[2]).toBeCloseTo(0);
+
+    TWEEN.update(1);
+    expect(color.getAttribute()[0]).toBeCloseTo(0.0);
+    expect(color.getAttribute()[1]).toBeCloseTo(1.0);
+    expect(color.getAttribute()[2]).toBeCloseTo(1.0);
+    expect(color.getCSSStyle()).toEqual("rgba(0,255,255,1)");
+  });
+});
